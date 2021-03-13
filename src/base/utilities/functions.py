33deab65ae7acd117cf1c70153507446ef71dbdb
Arult <universuen@gmail.com>
2021-03-14 04:00:38 +0800

    optimize file structure

--- src/base/utilities/functions.py
+++ src/base/utilities/functions.py
@@ -15,7 +15,7 @@ def get_html(url: str) -> bytes:
     """
     # 如果是第一次调用，需要读取User-Agents列表到ua_pool中
     if not hasattr(get_html, 'ua_pool'):
-        with open('base\\user_agents.json', 'r') as f:
+        with open('base\\utilities\\user_agents.json', 'r') as f:
             get_html.ua_pool = load(f)['user-agents']
 
     # 随机从ua_pool中取出一条user-agent
